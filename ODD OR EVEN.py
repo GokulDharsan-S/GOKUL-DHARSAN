448de0ea7d3c126f6a30705be6a6446d014d0024
GokulDharsan-S <113195662+GokulDharsan-S@users.noreply.github.com>
2022-10-07 18:44:43 +0530

    Add files via upload

---
+++ ODD OR EVEN.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+def oddoreven(n):
+    if n%2==0: print("It is an even number")
+    else:print("It is an odd number")
+
+oddoreven(int(input()))
+
+
+# In[ ]:
+
+
+
+
